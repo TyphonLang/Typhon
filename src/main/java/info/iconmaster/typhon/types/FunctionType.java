b32a7c6b484ba47e78933e83989fb9f1a00365e0
Iconmaster <crazybonesj@yahoo.com>
2017-08-22 19:14:01 -0400

    Added a helpful stub

--- src/main/java/info/iconmaster/typhon/types/FunctionType.java
+++ src/main/java/info/iconmaster/typhon/types/FunctionType.java
@@ -31,4 +31,47 @@ public class FunctionType extends Type {
 	public List<TemplateArgument> getTemplate() {
 		return template;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((argTypes == null) ? 0 : argTypes.hashCode());
+		result = prime * result + ((retTypes == null) ? 0 : retTypes.hashCode());
+		result = prime * result + ((template == null) ? 0 : template.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FunctionType other = (FunctionType) obj;
+		if (argTypes == null) {
+			if (other.argTypes != null)
+				return false;
+		} else if (!argTypes.equals(other.argTypes))
+			return false;
+		if (retTypes == null) {
+			if (other.retTypes != null)
+				return false;
+		} else if (!retTypes.equals(other.retTypes))
+			return false;
+		if (template == null) {
+			if (other.template != null)
+				return false;
+		} else if (!template.equals(other.template))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public boolean canCastTo(TypeRef a, TypeRef b) {
+		// TODO: function types need to be castable to each other
+		return super.canCastTo(a, b);
+	}
 }
